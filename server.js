ce5c696d40869379766c1895d98ea9105ddc18a5
Klaras Garink <granitxhaka897@gmail.com>
2025-07-02 18:37:59 +0700

    Second Commit

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}));
 app.use(bodyParser.json());
+app.use(express.json());
 
 // Database connection
 const dbConfig = {
@@ -35,6 +40,10 @@ pool.getConnection()
 
 // Routes
 // Get all projects
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Halo dari backend!' });
+});
+
 app.get('/api/projects', async (req, res) => {
   try {
     const [rows] = await pool.query('SELECT * FROM projects');
